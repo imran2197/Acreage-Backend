89d5e78e3591c277e1e1a2c50f8621901c99ad5d
Md Imran <samimdimran786@gmail.com>
2024-06-27 11:23:34 +0530

    commented s3 bucket for deployment fix

--- src/Routes/propertyRoutes.js
+++ src/Routes/propertyRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const propertyController = require("../controllers/propertyController");
-const { uploadToS3 } = require("../middlewares/uploadToS3");
+// const { uploadToS3 } = require("../middlewares/uploadToS3");
 
 router.post("/userProperties", propertyController.getUserProperties);
 router.post("/getTrackData", propertyController.getTrackData);
 router.post("/createTrackData", propertyController.createTrackData);
 router.post("/updateTrackData", propertyController.updateTrackData);
 router.post("/postProperty", propertyController.postProperty);
-router.post("/uploadImages", uploadToS3, propertyController.uploadImagesToS3);
-router.post("/deleteImage", propertyController.deleteImageFromS3);
+// router.post("/uploadImages", uploadToS3, propertyController.uploadImagesToS3);
+// router.post("/deleteImage", propertyController.deleteImageFromS3);
 
 module.exports = router;
--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -1,19 +1,19 @@
-const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+// const { S3Client, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const Property = require("../models/propertyDataModel");
 const collections = require("../index");
 const propertyModel = Property.propertyModel;
 
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const BUCKET_NAME = process.env.BUCKET_NAME;
-const s3 = new S3Client({
-  credentials: {
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    accessKeyId: process.env.ACCESS_KEY,
-  },
-  region: process.env.BUCKET_REGION,
-});
+// const BUCKET_NAME = process.env.BUCKET_NAME;
+// const s3 = new S3Client({
+//   credentials: {
+//     secretAccessKey: process.env.SECRET_ACCESS_KEY,
+//     accessKeyId: process.env.ACCESS_KEY,
+//   },
+//   region: process.env.BUCKET_REGION,
+// });
 
 const isNullOrUndefined = (val) => val === null || val === undefined;
 
@@ -190,96 +190,96 @@ exports.postProperty = async (req, res) => {
   }
 };
 
-exports.uploadImagesToS3 = async (req, res) => {
-  try {
-    const phoneNumber = Number(req.body.phoneNumber);
-    if (!isNullOrUndefined(phoneNumber)) {
-      const existingUser = await collections.userCollection.findOne({
-        phoneNumber,
-      });
-      if (!isNullOrUndefined(existingUser)) {
-        const existingTrackData = await collections.trackCollection.findOne({
-          phoneNumber,
-        });
-        if (!isNullOrUndefined(existingTrackData)) {
-          if (req.files.length > 0) {
-            req.files.map(async (file) => {
-              await collections.trackCollection.updateOne(
-                { phoneNumber },
-                { $push: { images: file.location } }
-              );
-            });
-            res.send({
-              statusCode: 200,
-              message: "Trackdata updated successfully.",
-            });
-          }
-        } else {
-          res.send({
-            statusCode: 404,
-            message: "No trackdata found.",
-          });
-        }
-      } else {
-        res.send({
-          statusCode: 401,
-          message: "User does not exist. Please login and try again.",
-        });
-      }
-    } else {
-      res.send({ statusCode: 401, message: "Please login and try again." });
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("Server Error");
-  }
-};
+// exports.uploadImagesToS3 = async (req, res) => {
+//   try {
+//     const phoneNumber = Number(req.body.phoneNumber);
+//     if (!isNullOrUndefined(phoneNumber)) {
+//       const existingUser = await collections.userCollection.findOne({
+//         phoneNumber,
+//       });
+//       if (!isNullOrUndefined(existingUser)) {
+//         const existingTrackData = await collections.trackCollection.findOne({
+//           phoneNumber,
+//         });
+//         if (!isNullOrUndefined(existingTrackData)) {
+//           if (req.files.length > 0) {
+//             req.files.map(async (file) => {
+//               await collections.trackCollection.updateOne(
+//                 { phoneNumber },
+//                 { $push: { images: file.location } }
+//               );
+//             });
+//             res.send({
+//               statusCode: 200,
+//               message: "Trackdata updated successfully.",
+//             });
+//           }
+//         } else {
+//           res.send({
+//             statusCode: 404,
+//             message: "No trackdata found.",
+//           });
+//         }
+//       } else {
+//         res.send({
+//           statusCode: 401,
+//           message: "User does not exist. Please login and try again.",
+//         });
+//       }
+//     } else {
+//       res.send({ statusCode: 401, message: "Please login and try again." });
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).send("Server Error");
+//   }
+// };
 
-exports.deleteImageFromS3 = async (req, res) => {
-  try {
-    const { phoneNumber, filename, imageName } = req.body;
-    try {
-      if (!isNullOrUndefined(phoneNumber)) {
-        await s3.send(
-          new DeleteObjectCommand({ Bucket: BUCKET_NAME, Key: filename })
-        );
-        const existingUser = await collections.userCollection.findOne({
-          phoneNumber,
-        });
-        if (!isNullOrUndefined(existingUser)) {
-          const existingTrackData = await collections.trackCollection.findOne({
-            phoneNumber,
-          });
-          if (!isNullOrUndefined(existingTrackData)) {
-            await collections.trackCollection.updateOne(
-              { phoneNumber },
-              { $pull: { images: imageName } }
-            );
-            res.send({
-              statusCode: 200,
-              message: "Image deleted successfully.",
-            });
-          } else {
-            res.send({
-              statusCode: 404,
-              message: "No trackdata found.",
-            });
-          }
-        } else {
-          res.send({
-            statusCode: 401,
-            message: "User does not exist. Please login and try again.",
-          });
-        }
-      } else {
-        res.send({ statusCode: 401, message: "Please login and try again." });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-    }
-  } catch (err) {
-    console.log(err);
-    return res.status(500).send("Server Error");
-  }
-};
+// exports.deleteImageFromS3 = async (req, res) => {
+//   try {
+//     const { phoneNumber, filename, imageName } = req.body;
+//     try {
+//       if (!isNullOrUndefined(phoneNumber)) {
+//         await s3.send(
+//           new DeleteObjectCommand({ Bucket: BUCKET_NAME, Key: filename })
+//         );
+//         const existingUser = await collections.userCollection.findOne({
+//           phoneNumber,
+//         });
+//         if (!isNullOrUndefined(existingUser)) {
+//           const existingTrackData = await collections.trackCollection.findOne({
+//             phoneNumber,
+//           });
+//           if (!isNullOrUndefined(existingTrackData)) {
+//             await collections.trackCollection.updateOne(
+//               { phoneNumber },
+//               { $pull: { images: imageName } }
+//             );
+//             res.send({
+//               statusCode: 200,
+//               message: "Image deleted successfully.",
+//             });
+//           } else {
+//             res.send({
+//               statusCode: 404,
+//               message: "No trackdata found.",
+//             });
+//           }
+//         } else {
+//           res.send({
+//             statusCode: 401,
+//             message: "User does not exist. Please login and try again.",
+//           });
+//         }
+//       } else {
+//         res.send({ statusCode: 401, message: "Please login and try again." });
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).send("Internal Server Error");
+//     }
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).send("Server Error");
+//   }
+// };
